98258ff5d256ff07309d133395e79812ff367898
萩原辰哉 <hagi@hagiharatatsuyanoMacBook-Pro.local>
2020-07-06 19:55:04 +0900

    update

--- create_indexmap/pascal_voc_classID.py
+++ create_indexmap/pascal_voc_classID.py
@@ -1,25 +1,15 @@
 from enum import IntEnum, Enum
 
 class Class_ID(IntEnum):
-    ID1_aeroplane = 1
-    ID2_bicycle = 2
-    ID3_bird = 3
-    ID4_boat = 4
-    ID5_bottle = 5
-    ID6_bus = 6
-    ID7_car= 7
-    ID8_cat= 8
-    ID9_chair = 9
-    ID10_cow= 10
-    ID11_diningtable= 11
-    ID12_dog = 12
-    ID13_horse= 13
-    ID14_motorbike= 14
-    ID15_person = 15
-    ID16_pottedplant = 16
-    ID17_sheep= 17
-    ID18_sofa = 18
-    ID19_train = 19
-    ID20_tvmonitor = 20
-    ID21_void = 21
+    ID1_car = 1
+    ID2_signal = 2
+    ID3_pedestrian = 3
+    ID4_lane = 4
+    ID5_bus = 5
+    ID6_truck= 6
+    ID7_motorbike= 7
+    ID8_signs = 8
+    ID9_ground= 9
+    ID10_sidewalk = 10
+    
 
--- create_indexmap/pascal_voc_indexmap.py
+++ create_indexmap/pascal_voc_indexmap.py
@@ -6,106 +6,100 @@ import os
 import random
 import numpy as np
 import cv2
-from natsort import natsorted
 import matplotlib.pyplot as plt
 from pascal_voc_classID import Class_ID
+import glob
 
-
-ID1_aeroplane = (128, 0, 0)
-ID2_bicycle = (0, 128, 0)
-ID3_bird = (0,128,128)
-ID4_boat = (128,0,0)
-ID5_bottle = (128,0,128)
-ID6_bus = (128,128,0)
-ID7_car= (128,128,128)
-ID8_cat= (0,0,64)
-ID9_chair = (0,0,192)
-ID10_cow= (0,128,64)
-ID11_diningtable= (0,128,192)
-ID12_dog = (128,0,64)
-ID13_horse= (128,0,192)
-ID14_motorbike= (128,128,64)
-ID15_person = (128,128,192)
-ID16_pottedplant = (0,64,0)
-ID17_sheep= (0,64,128)
-ID18_sofa = (0,192,0)
-ID19_train = (0,192,128)
-ID20_tvmonitor = (0,64,128)
-ID21_void = (12,64,128)
+class Indexmaps:
+    def __init__(self):
+        self.ID1 = (0, 0, 255)
+        self.ID2 = (255, 255, 0)
+        self.ID3 = (255, 0, 0)
+        self.ID4 = (69, 47, 142)
+        self.ID5 = (193, 214, 0)
+        self.ID6 = (180, 0, 129)
+        self.ID7 = (65, 166, 1)
+        self.ID8 = (255, 134, 0)
+        self.ID9 = (136, 45, 66)
+        self.ID10 = (0, 255, 255)
 
     
-def make_indexmap(img, base_indexmap):
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID1_aeroplane, Class_ID.ID1_aeroplane)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID2_bicycle, Class_ID.ID2_bicycle)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID3_bird, Class_ID.ID3_bird)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID4_boat, Class_ID.ID4_boat)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID5_bottle, Class_ID.ID5_bottle)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID6_bus, Class_ID.ID6_bus)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID7_car, Class_ID.ID7_car)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID8_cat, Class_ID.ID8_cat)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID9_chair, Class_ID.ID9_chair)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID10_cow, Class_ID.ID10_cow)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID11_diningtable, Class_ID.ID11_diningtable)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID12_dog, Class_ID.ID12_dog)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID13_horse, Class_ID.ID13_horse)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID14_motorbike, Class_ID.ID14_motorbike)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID15_person, Class_ID.ID15_person)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID16_pottedplant, Class_ID.ID16_pottedplant)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID17_sheep, Class_ID.ID17_sheep)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID18_sofa, Class_ID.ID18_sofa)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID19_train, Class_ID.ID19_train)
-    base_indexmap = mask_to_indexmap(img, base_indexmap, ID20_tvmonitor, Class_ID.ID20_tvmonitor)
-    indexmap = mask_to_indexmap(img, base_indexmap, ID21_void, Class_ID.ID21_void)
-    
-    return indexmap
+    def make_indexmap(self, img, base_indexmap):
+        base_indexmap = self.mask_to_indexmap(img, base_indexmap, self.ID1, Class_ID.ID1_car)
+        base_indexmap = self.mask_to_indexmap(img, base_indexmap, self.ID2, Class_ID.ID2_signal)
+        base_indexmap = self.mask_to_indexmap(img, base_indexmap, self.ID3, Class_ID.ID3_pedestrian)
+        base_indexmap = self.mask_to_indexmap(img, base_indexmap, self.ID4, Class_ID.ID4_lane)
+        base_indexmap = self.mask_to_indexmap(img, base_indexmap, self.ID5, Class_ID.ID5_bus)
+        base_indexmap = self.mask_to_indexmap(img, base_indexmap, self.ID6, Class_ID.ID6_truck)
+        base_indexmap = self.mask_to_indexmap(img, base_indexmap, self.ID7, Class_ID.ID7_motorbike)
+        base_indexmap = self.mask_to_indexmap(img, base_indexmap, self.ID8, Class_ID.ID8_signs)
+        base_indexmap = self.mask_to_indexmap(img, base_indexmap, self.ID9, Class_ID.ID9_ground)
+        indexmap = self.mask_to_indexmap(img, base_indexmap, self.ID10, Class_ID.ID10_sidewalk)
+        
+        return indexmap
 
-def mask_to_indexmap(img, base_indexmap, class_color, class_id):
-    h, w, _ = np.shape(img)
-    masks = np.zeros([h, w, 3])  # (366, 500, 3)
-    for x in range(h):
-        for y in range(w):
-            b, g, r = img[x, y]
-            if (b, g, r) == class_color:
-                masks[x, y] = class_id
-    base_indexmap[masks == class_id] = class_id
-    return base_indexmap
+    def mask_to_indexmap(self, img, base_indexmap, class_color, class_id):
+        h, w, _ = np.shape(img)
+        masks = np.zeros([h, w, 3])  # (366, 500, 3)
+        for x in range(h):
+            for y in range(w):
+                b, g, r = img[x, y]
+                if (b, g, r) == class_color:
+                    masks[x, y] = class_id
+        base_indexmap[masks == class_id] = class_id
+        return base_indexmap
 
-image_dir = 'drive/My Drive/JPEG_seg'
-anno_dir = 'drive/My Drive/SegmentationClass'
+def create_resized_img(img, H=608, W=416):
+      resize_img = cv2.resize(img, (H, W), interpolation=cv2.INTER_NEAREST)
+      #plt.imshow(resize_img),plt.show()
+      h, w, _ = np.shape(resize_img)
+      base_indexmap = np.zeros([h, w, 3])
+      return resize_img, base_indexmap
+
+def load_img(path):
+      img = cv2.imread(path)
+      img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+      return img
+
+
+image_dir = '/content/drive/My Drive/seg_train_images'
+anno_dir = '/content/drive/My Drive/seg_train_annotations'
 if not os.path.exists(image_dir) or not os.path.exists(anno_dir):
     print("ERROR!The folder is not exist")
     
-load_img_path = [os.path.join(image_dir, path) for path in os.listdir(image_dir)]
-sorted_img_path =[path for path in natsorted(load_img_path)]
+sorted_img_path = [os.path.join(image_dir, path) for path in sorted(glob.glob(image_dir+'/*.jpg'))]
+sorted_anno_path = [os.path.join(anno_dir, path) for path in sorted(glob.glob(anno_dir+'/*.png'))]
+print(len(sorted_img_path), len(sorted_anno_path))     
 
-load_anno_path = [os.path.join(anno_dir, path) for path in os.listdir(anno_dir)]
-sorted_anno_path =[path for path in natsorted(load_anno_path)]
-print(len(sorted_img_path), len(sorted_anno_path))   
+env = Indexmaps()
+anno_save_dir="/content/drive/My Drive/indexmap"
+jpg_save_dir = "/content/drive/My Drive/resized_train"
 
-for idx, (anno_path, img_path) in enumerate(zip(sorted_anno_path[:20], sorted_img_path)):
+for idx, (anno_path, jpg_path) in enumerate(zip(sorted_anno_path, sorted_img_path)):
     # annos
-    img = cv2.imread(anno_path)
-　　 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    save_name, _ = os.path.splitext(os.path.basename(anno_path))
+    img = load_img(anno_path)
 
-    print(img.shape, np.unique(img))
-    h, w, _ = np.shape(img)
-    img = cv2.resize(img, (int(w/4), int(h/4)), interpolation=cv2.INTER_NEAREST)
-    plt.imshow(img),plt.show()
-    nh, nw, _ = np.shape(img)
-    base_indexmap = np.zeros([nh, nw, 3])
+    # create indexmap
+    resize_img, base_indexmap = create_resized_img(img, H=608, W=416)
 
-
-    indexmap = make_indexmap(img, base_indexmap)
-    indexmap = cv2.resize(indexmap, (608, 416), interpolation=cv2.INTER_NEAREST)
+    print(resize_img.shape, base_indexmap.shape)
+    indexmap = env.make_indexmap(resize_img, base_indexmap)
     print(np.unique(indexmap), indexmap.shape)
     plt.imshow(indexmap),plt.show()
 
+    # save anno
+    cv2.imwrite(os.path.join(anno_save_dir, save_name + ".png"), indexmap)
 
-    anno_save_dir="drive/My Drive/voc_anno"
-    cv2.imwrite(os.path.join(anno_save_dir, str(idx) + "_indexmap.png"), indexmap)
+    # save jpg
+    print(jpg_path)
+    jpg = load_img(jpg_path)
+    resized_jpg, _ = create_resized_img(jpg, H=608, W=416)
+    plt.imshow(resized_jpg),plt.show()
+    cv2.imwrite(os.path.join(jpg_save_dir, save_name + ".jpg"), resized_jpg)
 
-    # imgs
-    jpg = cv2.imread(img_path)
-    jpg = cv2.resize(jpg, (608, 416), interpolation=cv2.INTER_NEAREST)
-    jpg_save_dir = "drive/My Drive/voc_jpeg"
-    cv2.imwrite(os.path.join(jpg_save_dir, str(idx) + "_indexmap.jpg"), jpg)
+    if idx==50:
+      break
+
+
+
